f8cf7b00292e8fcfe6a666b8e4c09e39d4791e1c
Evan Kesten <evan@joincabinet.com>
2024-04-04 15:23:28 -0400

    fix: disable merge button if squash is already selected

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,18 +1,21 @@
-function checkBranch(){
-    chrome.storage.sync.get(({branchesList:[]}), (data)=>{  
-        const targetBranch = document.querySelector("span.commit-ref span.css-truncate-target").innerText
-        if (data.branchesList.includes(targetBranch)){
-            const button = document.querySelector("button.js-merge-box-button-squash")
-            if (button){
-                button.disabled=true
-            }else{
-                setTimeout(checkBranch,125)
+function checkBranch() {
+    chrome.storage.sync.get(({ branchesList: [] }), (data) => {  
+        const targetBranch = document.querySelector("span.commit-ref span.css-truncate-target").innerText;
+        if (data.branchesList.includes(targetBranch)) {
+            const selectItem = document.querySelector("button.js-merge-box-button-squash");
+            const mergeButton = document.querySelector("button.btn-group-squash");
+
+            if (selectItem && mergeButton) {
+                selectItem.disabled = true;
+                mergeButton.disabled = true;
+            } else {
+                setTimeout(checkBranch, 125);
             }
         }    
-    })
+    });
 }
 
 
 window.onload = function() {
-    setTimeout(checkBranch, 125)
+    setTimeout(checkBranch, 125);
 }
